aca20c2b9754fcabab893320eb24f4fc232975c0
SuzukiDaishi <zukky.rikugame@gmail.com>
2021-11-05 12:48:10 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as THREE from "three";
 import { VRButton, } from 'three/examples/jsm/webxr/VRButton'
+import { OrbitControls, } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { VRM } from '@pixiv/three-vrm'
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.xr.enabled = true
   // レンダラーのサイズを設定
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,17 +16,35 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // シーンを作成
   const scene = new THREE.Scene();
+  const loader = new GLTFLoader()
 
   // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 10000);
-  camera.position.set(0, 0, 1000);
+  let camera: THREE.Camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 10000)
+  const cameraContainer = new THREE.Object3D()
+  cameraContainer.add(camera)
+  cameraContainer.position.set(0, 0, 5000)
+  scene.add(cameraContainer)
+
+  // コントローラを作成
+  const orbitControls = new OrbitControls(camera,renderer.domElement);
+  orbitControls.screenSpacePanning = true
 
   // 箱を作成
   const geometry = new THREE.TorusGeometry(300, 100, 64, 100);
   const material = new THREE.MeshToonMaterial({ color: 0xff0000 });
   const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.z = -5;
+  mesh.position.set(0, 20, 20)
   scene.add(mesh);
+
+  new GLTFLoader().load('../assets/model.vrm', (gltf) => {
+      VRM.from( gltf ).then( 
+      ( vrm ) => {
+        scene.add(vrm.scene);
+        console.log(vrm);
+      },
+      ( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
+    )
+  })
 
   // 平行光源を生成
   const light = new THREE.DirectionalLight(0xffffff);
@@ -31,15 +52,18 @@ window.addEventListener("DOMContentLoaded", () => {
   scene.add(light);
 
   const tick = (): void => {
-    requestAnimationFrame(tick)
+    mesh.rotation.x += 0.05;
+    mesh.rotation.y += 0.05;
+    
     renderer.setAnimationLoop(() => {
-      mesh.rotation.x += 0.05;
-      mesh.rotation.y += 0.05;
+      orbitControls.update()
       // 描画
       renderer.render(scene, camera);
     })
+    requestAnimationFrame(tick)
   };
   tick();
 
   console.log("Hello Three.js");
 });
+
